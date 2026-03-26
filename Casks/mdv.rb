9b9e0201de433a02d71a31f824bd78b138f55221
Yoshiteru Negishi <negipo@gmail.com>
2026-03-26 09:18:31 +0900

    feat: add mdv cask

---
+++ Casks/mdv.rb
@@ -0,0 +1,26 @@
+cask "mdv" do
+  version "2026.03.25.1"
+  sha256 "089222ed24d0c690a0e26076a5bea0dff29a294395d5396e9375da2ccf6c515d"
+
+  url "https://github.com/negipo/mdv/releases/download/v#{version}/mdv-#{version}-macos.dmg"
+  name "mdv"
+  desc "Markdown viewer for macOS"
+  homepage "https://github.com/negipo/mdv"
+
+  depends_on macos: ">= :sonoma"
+
+  app "mdv.app"
+  binary "#{appdir}/mdv.app/Contents/Resources/bin/mdv"
+
+  caveats <<~EOS
+    mdv is not signed with an Apple Developer ID.
+    After installation, run the following to remove the quarantine attribute:
+      xattr -dr com.apple.quarantine /Applications/mdv.app
+  EOS
+
+  zap trash: [
+    "~/Library/Application Support/mdv",
+    "~/Library/Preferences/com.negipo.mdv.plist",
+    "~/Library/Caches/com.negipo.mdv",
+  ]
+end
---
+++ README.md
@@ -0,0 +1,16 @@
+# homebrew-tap
+
+Homebrew tap for negipo's projects.
+
+## mdv
+
+Markdown viewer for macOS.
+
+### Install
+
+    brew install --cask negipo/tap/mdv
+    xattr -dr com.apple.quarantine /Applications/mdv.app
+
+### Uninstall
+
+    brew uninstall --cask mdv
